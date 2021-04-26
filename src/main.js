ce87247143bc37081d3c06974d0548577149be62
Quincy West <rakaneth@gmail.com>
2021-04-26 16:44:49 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+import { Terminal } from 'malwoden';
+import img from './agm_16x16.png';
+
+window.onload = () => {
+    const mountNode = document.getElementById('canvas');
+
+    const displayOptions = {
+        width: 50,
+        height: 30,
+        imageURL: img,
+        mountNode,
+        charWidth: 16,
+        charHeight: 16,
+    };
+    
+    const terminal = new Terminal.RetroTerminal(displayOptions);
+    
+    terminal.clear();
+    terminal.writeAt({x: 0, y: 0}, "Hello Malwoden!");
+    terminal.render();
+}
+
+
+

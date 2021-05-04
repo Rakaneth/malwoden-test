e77f7437a295a674405a14d3547a28819c117cdd
Quincy West <rakaneth@gmail.com>
2021-05-04 19:47:27 -0400

    update msg screen to scale with terminal

--- src/screens/msgscreen.js
+++ src/screens/msgscreen.js
@@ -24,8 +24,8 @@ export default class MessageScreen extends Screen {
         let i=0;
         GUI.box(this._terminal, {
             origin: {x: 0, y: 0},
-            width: 49,
-            height: 34,
+            width: this._terminal.width-1,
+            height: this._terminal.height-1,
             title: "Messages",
         });
 
@@ -39,8 +39,8 @@ export default class MessageScreen extends Screen {
         //idx remains undefined if this._downIdx is 0
         //which makes slice return the whole array
         for (let msg of GameManager.msgs.slice(0, idx).reverse()) {
-            let list = wrap(msg, 47);
-            if (list.length + i > 33) break;
+            let list = wrap(msg, this._terminal.width-3);
+            if (list.length + i > this._terminal.height-3) break;
             for (let s of list) {
                 this._terminal.writeAt({x: 1, y: 1+i}, s);
                 i++;
